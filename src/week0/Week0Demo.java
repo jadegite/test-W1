3794bf6db424f5a58b54171455859729499c5f3f
jadegite <101847100+jadegite@users.noreply.github.com>
2022-06-25 21:23:05 -0400

    first commit being made

---
+++ src/week0/Week0Demo.java
@@ -0,0 +1,16 @@
+package week0;
+
+public class Week0Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printAMessage("hi to the new cohort!!!");
+
+	}
+
+	private static void printAMessage(String message) {
+		// TODO Auto-generated method stub
+		System.out.println(message);
+	}
+
+}
